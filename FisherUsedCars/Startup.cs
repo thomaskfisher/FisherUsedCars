2305c5f393341677326877adb8078efea038b510
Thomas Fisher <thomas.kimball.fisher@gmail.com>
2016-12-11 10:45:11 -0700

    First Commit

---
+++ FisherUsedCars/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FisherUsedCars.Startup))]
+namespace FisherUsedCars
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
